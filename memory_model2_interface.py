2e16d5b94e26d06e742b96f3734ade10b934f765
teddy.ceres <thedros@uw.edu>
2020-01-26 16:43:21 -0800

    added semi-functioning test phase

--- memory_model2_interface.py
+++ memory_model2_interface.py
@@ -56,23 +56,42 @@ def present_feedback():
     global i
     global current_response
     global accuracy
-    
-    feedback = 'no'
-     
-    # check if response matches the appropriate key for the current stimulus in cue
-    #need list of correct responses
-    if current_response[i] == cor_resps[i]:
-        feedback = 'yes'
-        accuracy[i] = 1
-    
-    chunks = actr.define_chunks(['isa', 'feedback', 'feedback',feedback])
-    actr.set_buffer_chunk('visual', chunks[0])
-    print("Feedback given: ", feedback )
+
+    if i > 10:
+    # this tests whether the current trial is a test phase. This portion does not present feedback but checks accuracy
+        if current_response[i] == cor_resps[i]:
+            accuracy[i] = 1
+            actr.schedule_event_relative(1, 'present_stim')
+            print("Feedback given: None, test phase" )
   
-    #increase index for next stimulus
+    else:
+    # This runs for learning phase. This portion presents feedback
+        feedback = 'no'
+        
+    # check if response matches the appropriate key for the current stimulus in cue
+        if current_response[i] == cor_resps[i]:
+            feedback = 'yes'
+            accuracy[i] = 1
+    # present feedback    
+        chunks = actr.define_chunks(['isa', 'feedback', 'feedback',feedback])
+        actr.set_buffer_chunk('visual', chunks[0])
+        print("Feedback given: ", feedback )
+        
+        if i == 2: #lastLearnTrial:
+            print("BREAK HERE")
+            actr.schedule_event_relative(300, 'present_stim')
+        else:
+            actr.schedule_event_relative(1, 'present_stim')
+#increase index for next stimulus
     i = i + 1
-    actr.schedule_event_relative(1, 'present_stim')
-    
+   
+
+   
+
+
+	
+
+
 
 # This function builds ACT-R representations of the python functions
 
@@ -90,23 +109,26 @@ def model_loop():
     
     #waits for a key press?
     actr.monitor_command("output-key", 'get_response')
-    actr.run(200)
+    actr.run(2000)
     
    
 #model_loop()
 #
-nsimulations = np.arange(3) #set the number of simulations "subjects"
+nsimulations = np.arange(1) #set the number of simulations "subjects"
 for x in nsimulations:
     actr.reset()
     #Stimuli to be used and exp parameters
     stims_3 = ['cup','bowl','plate']
     stims_6 = ['hat','gloves','shoes', 'shirt', 'jacket', 'jeans']
-    nPresentations = 12
-    nTrials = nPresentations * 9 #3 #for sets size three experiment/block
+    test_stims = ["bowl", "shirt", "jeans", "plate", "cup", "jacket"] #Each stimulus was presented 4 times during test
+    nPresentations = 12 #learning phase, items were presented 12-14 times during learning
+    nTestPresentations = 4
+    nTrials = (nPresentations * 9) + (nTestPresentations * np.size(test_stims))  #3 #for sets size three experiment/block
 
-    #associated responses (these are arbitrary)
-    stims_3_resps = ['j', 'j', 'l'];
-    stims_6_resps = ['k','k', 'j', 'j', 'l', 'l'];
+    #associated responses (matches Collins' patterns of response-stim associations)
+    stims_3_resps = ['j', 'j', 'l']
+    stims_6_resps = ['k','k', 'j', 'j', 'l', 'l']
+    test_resps    = ["j", "j", "l", "l", "j", "l"]
 
     #generate stimult to present to model **Edit as needed **
 
@@ -120,6 +142,7 @@ for x in nsimulations:
     rnd.shuffle(stims_temp3)
 
     stims3, cor_resps3 = zip(*stims_temp3)
+    
     # 6 set block
     stims_temp6 = list( zip(np.repeat(stims_6, 12).tolist(),
         np.repeat(stims_6_resps, 12).tolist()
@@ -127,13 +150,25 @@ for x in nsimulations:
     rnd.shuffle(stims_temp6)
     stims6, cor_resps6 = (zip(*stims_temp6))
 
-    stims = stims3 + stims6
-    cor_resps = cor_resps3 + cor_resps6
+   
+   # test phase 
+    test_temp = list(zip(np.repeat(test_stims, 4).tolist(),
+	np.repeat(test_resps, 4).tolist()
+  	  ))
 
+    rnd.shuffle(test_temp)
+    teststims, cor_testresps = (zip(*test_temp))
+     
+    # concat all stimuli and responses together to present
+    
+    stims = stims3 + stims6 + teststims
+    cor_resps = cor_resps3 + cor_resps6 + cor_testresps
+  
     #variables needed
     chunks = None
     current_response  = np.repeat('x', nTrials * 2).tolist() #multiply by 2 for number of blocks
     accuracy = np.repeat(0, nTrials).tolist()
+    lastLearnTrial = np.size(stims3 + stims6) -1
     
     i = 0
     win = None
@@ -148,8 +183,10 @@ for x in nsimulations:
     #actr.set_parameter_value(":bll",0.9) 
 
 
+## data analysis and plotting
 
-    if True :
+
+    if False :
         print('mean accuracy: ', np.mean(accuracy))
 
 
@@ -170,7 +207,7 @@ for x in nsimulations:
         #pyplot.show()
 
     # 6 items plot (will be fixed later)
-    if True : 
+    if False : 
         hat_presented = np.where(stims_array == 'hat') 
         gloves_presented = np.where(stims_array == 'gloves') 
         shoes_presented = np.where(stims_array == 'shoes') 
