d058b944ee9241697f47125ccc1a912a7e994c9b
theodros.h <theodros@uw.edu>
2020-01-27 21:18:21 -0800

    nested for loops for param estimation :|

--- integrated_model_interface.py
+++ integrated_model_interface.py
@@ -16,10 +16,6 @@ from matplotlib import pyplot
 
 
 
-
-
-
-
 #Load model
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 actr.load_act_r_model(os.path.join(curr_dir, "integrated-model.lisp"))
@@ -109,165 +105,176 @@ def model_loop():
 
 
 ## execute model and simulate data
+
 #parameter ranges for simulation
 bll_param   = [0.3,0.5, 0.7]#np.arange(0.3, 0.71,0.01)   # decay rate of declarative memory
 alpha_param = np.arange(0.17, 0.35, 0.01) # learning rate of the RL utility selection
-#egs_param   = #amount of noise added to the RL utility selection
-#imaginal_param = #simulates working memory as attentional focus 
+egs_param   = np.arange(0.2, 0.9, 0.1) # amount of noise added to the RL utility selection
+imag_param = np.arange(0,6.5,0.5) #simulates working memory as attentional focus 
+ans_param   = np.arange(0.2, 0.9, 0.1) #parameter for noise in dec. memory activation. Range recommended by ACTR manual. 
+
 
 nsimulations = np.arange(1) #set the number of simulations "subjects"
+for ans in ans_param:
+    actr.set_parameter_value(":ans", ans)
 
 
-for bll in bll_param:
+    for imag in imag_param:
+        actr.set_parameter_value(":imaginal_activation", imag)
+        for egs in egs_param:
+            actr.set_parameter_value(":egs", egs)
 
-    for nsim in nsimulations:
-        actr.reset()
+            for alpha in alpha_param:
+                actr.set_parameter_value(":alpha", alpha) 
+                
+                for bll in bll_param:
 
-        #set parameters
-        #actr.set_parameter_value(":ans",0.9) 
-        actr.set_parameter_value(":bll",bll) 
+                    for nsim in nsimulations:
+                        actr.reset()
 
-
-
-        #Stimuli to be used and exp parameters
-        stims_3 = ['cup','bowl','plate']
-        stims_6 = ['hat','gloves','shoes', 'shirt', 'jacket', 'jeans']
-        test_stims = ["bowl", "shirt", "jeans", "plate", "cup", "jacket"] #Each stimulus was presented 4 times during test
-        nPresentations = 12 #learning phase, items were presented 12-14 times during learning
-        nTestPresentations = 4
-        nTrials = (nPresentations * 9) + (nTestPresentations * np.size(test_stims))  #3 #for sets size three experiment/block
-
-        #associated responses (matches Collins' patterns of response-stim associations)
-        stims_3_resps = ['j', 'j', 'l']
-        stims_6_resps = ['k','k', 'j', 'j', 'l', 'l']
-        test_resps    = ["j", "j", "l", "l", "j", "l"]
-
-        #generate stimult to present to model **Edit as needed **
-
-        #this shuffles both lists, stimuli and associated correct responses, in the same order
-
-        # 3 set block
-        stims_temp3 = list( zip(np.repeat(stims_3, 12).tolist(),
-                 np.repeat(stims_3_resps,12).tolist()
-                ))
-
-        rnd.shuffle(stims_temp3)
-
-        stims3, cor_resps3 = zip(*stims_temp3)
-        
-        # 6 set block
-        stims_temp6 = list( zip(np.repeat(stims_6, 12).tolist(),
-            np.repeat(stims_6_resps, 12).tolist()
-            ))
-        rnd.shuffle(stims_temp6)
-        stims6, cor_resps6 = (zip(*stims_temp6))
-
-       
-       # test phase 
-        test_temp = list(zip(np.repeat(test_stims, 4).tolist(),
-    	np.repeat(test_resps, 4).tolist()
-      	  ))
-
-        rnd.shuffle(test_temp)
-        teststims, cor_testresps = (zip(*test_temp))
-         
-        # concat all stimuli and responses together to present
-        
-        stims = stims3 + stims6 + teststims
-        cor_resps = cor_resps3 + cor_resps6 + cor_testresps
-      
-        #variables needed
-        chunks = None
-        current_response  = np.repeat('x', nTrials * 2).tolist() #multiply by 2 for number of blocks
-        accuracy = np.repeat(0, nTrials).tolist()
-        lastLearnTrial = np.size(stims3 + stims6) -1
-        
-        i = 0
-        win = None
-        model_loop()
-
-## data analysis and plotting
-
-    if True :
-        print('mean accuracy: ', np.mean(accuracy))
-
-
-        stims_array = np.asarray(stims[0:lastLearnTrial + 1]) 
-        acc_array   = np.asarray(accuracy[0:lastLearnTrial + 1]) 
-
-        cup_presented   = np.where(stims_array == 'cup') 
-        bowl_presented  = np.where(stims_array == 'bowl') 
-        plate_presented = np.where(stims_array == 'plate') 
-
-        acc_by_presentation3 = np.mean([acc_array[cup_presented], acc_array[plate_presented], acc_array[bowl_presented]],0)
-        acc3 = pd.DataFrame(acc_by_presentation3)
-       # print("mean accuracy set 3: " , np.mean(acc_by_presentation3))
-       # print(acc_by_presentation3)
-        #plot 
-        pyplot.figure(dpi=120)
-        pyplot.title("bll ",)
-        sns.regplot(np.arange(12)+1, acc_by_presentation3, order=2, label="set_3")
-        #pyplot.show()
-
-    # 6 items plot (will be fixed later)
-    if True : 
-        hat_presented    = np.where(stims_array == 'hat') 
-        gloves_presented = np.where(stims_array == 'gloves') 
-        shoes_presented  = np.where(stims_array == 'shoes') 
-        shirt_presented  = np.where(stims_array == 'shirt') 
-        jacket_presented = np.where(stims_array == 'jacket') 
-        jeans_presented  = np.where(stims_array == 'jeans') 
-
-        acc_by_presentation6 = np.mean([acc_array[hat_presented], 
-            acc_array[gloves_presented], 
-            acc_array[shoes_presented],
-            acc_array[shirt_presented],
-            acc_array[jacket_presented],
-            acc_array[jeans_presented]], 0)
-        acc6=pd.DataFrame(acc_by_presentation6)
-        print("mean accuracy set6: ", np.mean(acc_by_presentation6))
-       
-        #plot 
-       # pyplot.figure(dpi=300)
-        sns.regplot(np.arange(12)+1, acc_by_presentation6, order=2,label="set_6")
-        pyplot.show()
-        
-        # plot test accuracy
-    if True:
-        test_array = np.asarray(stims[lastLearnTrial+1 : np.size(stims)]) 
-        test_acc_array   = np.asarray(accuracy[lastLearnTrial+1 : np.size(stims)]) 
-
-
-        cup_presented   = np.where(test_array == 'cup') 
-        bowl_presented  = np.where(test_array == 'bowl') 
-        plate_presented = np.where(test_array == 'plate') 
-
-        hat_presented    = np.where(test_array == 'hat') 
-        gloves_presented = np.where(test_array == 'gloves') 
-        shoes_presented  = np.where(test_array == 'shoes') 
-        shirt_presented  = np.where(test_array == 'shirt') 
-        jacket_presented = np.where(test_array == 'jacket') 
-        jeans_presented  = np.where(test_array == 'jeans') 
-
-        test_3 = pd.DataFrame(np.mean([test_acc_array[cup_presented], acc_array[plate_presented], acc_array[bowl_presented]],0))
-
-        test_6 = pd.DataFrame(np.mean([ 
-            test_acc_array[shirt_presented],
-            test_acc_array[jacket_presented],
-            test_acc_array[jeans_presented]], 0))
-
-        pyplot.figure(dpi=120)
-        sns.barplot(x=["set 3", "set 6"], y=[np.mean(test_3),np.mean(test_6)]) 
-        
+                        #set parameters
+                        #actr.set_parameter_value(":ans",0.9) 
+                        actr.set_parameter_value(":bll",bll) 
 
 
 
-    if False:
-        #save data to files
-        f3 = open("set3.csv", 'a')
-        f6 = open("set6.csv", 'a')
-        acc6.transpose().to_csv(f6, mode='a', header = False)
-        acc3.transpose().to_csv(f3, mode='a', header = False)
-        f3.close()
-        f6.close()
+                        #Stimuli to be used and exp parameters
+                        stims_3 = ['cup','bowl','plate']
+                        stims_6 = ['hat','gloves','shoes', 'shirt', 'jacket', 'jeans']
+                        test_stims = ["bowl", "shirt", "jeans", "plate", "cup", "jacket"] #Each stimulus was presented 4 times during test
+                        nPresentations = 12 #learning phase, items were presented 12-14 times during learning
+                        nTestPresentations = 4
+                        nTrials = (nPresentations * 9) + (nTestPresentations * np.size(test_stims))  #3 #for sets size three experiment/block
+
+                        #associated responses (matches Collins' patterns of response-stim associations)
+                        stims_3_resps = ['j', 'j', 'l']
+                        stims_6_resps = ['k','k', 'j', 'j', 'l', 'l']
+                        test_resps    = ["j", "j", "l", "l", "j", "l"]
+
+                        #generate stimult to present to model **Edit as needed **
+
+                        #this shuffles both lists, stimuli and associated correct responses, in the same order
+
+                        # 3 set block
+                        stims_temp3 = list( zip(np.repeat(stims_3, 12).tolist(),
+                                 np.repeat(stims_3_resps,12).tolist()
+                                ))
+
+                        rnd.shuffle(stims_temp3)
+
+                        stims3, cor_resps3 = zip(*stims_temp3)
+                        
+                        # 6 set block
+                        stims_temp6 = list( zip(np.repeat(stims_6, 12).tolist(),
+                            np.repeat(stims_6_resps, 12).tolist()
+                            ))
+                        rnd.shuffle(stims_temp6)
+                        stims6, cor_resps6 = (zip(*stims_temp6))
+
+                       
+                       # test phase 
+                        test_temp = list(zip(np.repeat(test_stims, 4).tolist(),
+                    	np.repeat(test_resps, 4).tolist()
+                      	  ))
+
+                        rnd.shuffle(test_temp)
+                        teststims, cor_testresps = (zip(*test_temp))
+                         
+                        # concat all stimuli and responses together to present
+                        
+                        stims = stims3 + stims6 + teststims
+                        cor_resps = cor_resps3 + cor_resps6 + cor_testresps
+                      
+                        #variables needed
+                        chunks = None
+                        current_response  = np.repeat('x', nTrials * 2).tolist() #multiply by 2 for number of blocks
+                        accuracy = np.repeat(0, nTrials).tolist()
+                        lastLearnTrial = np.size(stims3 + stims6) -1
+                        
+                        i = 0
+                        win = None
+                        model_loop()
+
+                ## data analysis and plotting
+
+                    if False :
+                        print('mean accuracy: ', np.mean(accuracy))
+
+
+                        stims_array = np.asarray(stims[0:lastLearnTrial + 1]) 
+                        acc_array   = np.asarray(accuracy[0:lastLearnTrial + 1]) 
+
+                        cup_presented   = np.where(stims_array == 'cup') 
+                        bowl_presented  = np.where(stims_array == 'bowl') 
+                        plate_presented = np.where(stims_array == 'plate') 
+
+                        acc_by_presentation3 = np.mean([acc_array[cup_presented], acc_array[plate_presented], acc_array[bowl_presented]],0)
+                        acc3 = pd.DataFrame(acc_by_presentation3)
+                       # print("mean accuracy set 3: " , np.mean(acc_by_presentation3))
+                       # print(acc_by_presentation3)
+                        #plot 
+                        pyplot.figure(dpi=120)
+                        pyplot.title("bll ",)
+                        sns.regplot(np.arange(12)+1, acc_by_presentation3, order=2, label="set_3")
+                        #pyplot.show()
+
+                    # 6 items plot (will be fixed later)
+                    if False : 
+                        hat_presented    = np.where(stims_array == 'hat') 
+                        gloves_presented = np.where(stims_array == 'gloves') 
+                        shoes_presented  = np.where(stims_array == 'shoes') 
+                        shirt_presented  = np.where(stims_array == 'shirt') 
+                        jacket_presented = np.where(stims_array == 'jacket') 
+                        jeans_presented  = np.where(stims_array == 'jeans') 
+
+                        acc_by_presentation6 = np.mean([acc_array[hat_presented], 
+                            acc_array[gloves_presented], 
+                            acc_array[shoes_presented],
+                            acc_array[shirt_presented],
+                            acc_array[jacket_presented],
+                            acc_array[jeans_presented]], 0)
+                        acc6=pd.DataFrame(acc_by_presentation6)
+                        print("mean accuracy set6: ", np.mean(acc_by_presentation6))
+                       
+                        #plot 
+                       # pyplot.figure(dpi=300)
+                        sns.regplot(np.arange(12)+1, acc_by_presentation6, order=2,label="set_6")
+                        pyplot.show()
+                        
+                        # plot test accuracy
+                    if False:
+                        test_array = np.asarray(stims[lastLearnTrial+1 : np.size(stims)]) 
+                        test_acc_array   = np.asarray(accuracy[lastLearnTrial+1 : np.size(stims)]) 
+
+
+                        cup_presented   = np.where(test_array == 'cup') 
+                        bowl_presented  = np.where(test_array == 'bowl') 
+                        plate_presented = np.where(test_array == 'plate') 
+
+                        hat_presented    = np.where(test_array == 'hat') 
+                        gloves_presented = np.where(test_array == 'gloves') 
+                        shoes_presented  = np.where(test_array == 'shoes') 
+                        shirt_presented  = np.where(test_array == 'shirt') 
+                        jacket_presented = np.where(test_array == 'jacket') 
+                        jeans_presented  = np.where(test_array == 'jeans') 
+
+                        test_3 = pd.DataFrame(np.mean([test_acc_array[cup_presented], acc_array[plate_presented], acc_array[bowl_presented]],0))
+
+                        test_6 = pd.DataFrame(np.mean([ 
+                            test_acc_array[shirt_presented],
+                            test_acc_array[jacket_presented],
+                            test_acc_array[jeans_presented]], 0))
+
+                        pyplot.figure(dpi=120)
+                        sns.barplot(x=["set 3", "set 6"], y=[np.mean(test_3),np.mean(test_6)]) 
+
+
+                    if False:
+                        #save data to files
+                        f3 = open("set3.csv", 'a')
+                        f6 = open("set6.csv", 'a')
+                        acc6.transpose().to_csv(f6, mode='a', header = False)
+                        acc3.transpose().to_csv(f3, mode='a', header = False)
+                        f3.close()
+                        f6.close()
 
