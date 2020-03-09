7d133dca07d9ca9ff8e4d25e682faeb159877e7f
theodros.ccdl <theodros@uw.edu>
2020-03-09 14:08:05 -0700

    shiny updates

--- param_explorer/server.R
+++ param_explorer/server.R
@@ -9,35 +9,35 @@
 library(shiny)
 library(jsonlite)
 
+dat <- fromJSON('../sim_data_first_half_param_space_030620_integr_model100sTEST.JSON')
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$distPlot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
-        dat <- jsonlite::fromJSON('../sim_data_first_half_param_space_030620_integr_model100sTEST.JSON')
        
-         #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-       al <- seq(0.05, 0.25, .05)
-    ##    b  <- seq(0.3, 0.7, 0.1)
-      #  an <- seq(0.1, 0.5, 0.1)
-        
-        
-        # draw the histogram with the specified number of bins
+              # draw the histogram with the specified number of bins
         #hist(x, breaks = bins, col = 'darkgray', border = 'white')
         plot(c(1:12),
              dat$set3_learn[
-                 dat$alpha==input$al & dat$bll==input$b & dat$ans==input$an & dat$input$egs==e & dat$imag == input$i
+                 dat$alpha==input$al & dat$bll==input$b & dat$ans==input$an & dat$input$egs==input$e & dat$imag == input$i
                  ]$`0`, 
              col = '#e41a1c',
-             cex=1,lwd=2,pch=19, xlab ='Stimulus presentations',
+             cex = 1,
+             lwd = 2,
+             pch = 19, 
+             xlab ='Stimulus presentations',
              ylab ='Accuracy'
              )
         points(c(1:12),
              dat$set6_learn[
-                 dat$alpha==input$al & dat$bll==input$b & dat$ans==input$an & dat$egs==e & dat$imag == input$i
+                 dat$alpha==input$al & dat$bll==input$b & dat$ans==input$an & dat$egs==input$e & dat$imag == input$i
                  ]$`0`, 
-             cex=1,lwd=2,pch=19, 
+             cex=1,
+             lwd=2,
+             pch=19
         )
         
     })
