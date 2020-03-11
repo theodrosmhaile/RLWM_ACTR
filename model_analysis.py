fb4fc5ff9436d704b81fea06b0d5421e91f5e1af
theodros.ccdl <theodros@uw.edu>
2020-03-11 15:39:56 -0700

    update

---
+++ model_analysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+import random as rnd
+import numpy as np
+import os
+import sys
+import string
+import pandas as pd
+import seaborn as sns 
+from matplotlib import pyplot
+import itertools
+
+
+# import subject data
+sdat = pd.read()
+# import model data
+# loop through subject data and check for fit against model data
