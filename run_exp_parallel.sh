286e0e5fc181e1a8222eaf4eb000dbc4ed0da625
Teddy Zufan <theodroshaile@me.com>
2020-07-08 18:49:37 -0700

    conflict fix

--- run_exp_parallel.sh
+++ run_exp_parallel.sh
@@ -25,7 +25,7 @@
 # -v mount shared location HOST:CONTAINER
 # -p port forwarding HOST:CONTAINER
 
-p=(25) # this will equal to the number of iterations planned, which will be the same as (p)orts. Can be input from stdin
+#p=(25) # this will equal to the number of iterations planned, which will be the same as (p)orts. Can be input from stdin
 
 #sim_param=(125) #This will vary by experiment. 125 for LTM model
 use_ports=(2650 2651 2652 2653 2654 2655 2656 2657 2658) #"$(echo {2650..2653})" 
@@ -34,7 +34,7 @@ use_ports=(2650 2651 2652 2653 2654 2655 2656 2657 2658) #"$(echo {2650..2653})"
 fromi=(0 15 30 45 55 70 85 100 115)
 Toi=(14 29 44 54 69 84 99 114 124)
 
-for (( i = 0; i < 9; i++ )); do
+for (( i = 0; i < 5; i++ )); do
 	#statements
 
 #This starts the ACTR server in the background
